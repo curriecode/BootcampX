bcd4e43c1784d5ebfd5efe4d7809072e838a1d78
curriecode <melaniecurrie01@gmail.com>
2019-10-15 13:55:30 -0700

    created: new tables , added: 4_queries folder,  completed: 1 query.

---
+++ 4_queries/total_student_assistance_requests.sql
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+-- Get the total number of assistance_requests
+-- for a student.
+
+-- Select the student's name and the total 
+-- assistance requests.
+-- Since this query needs to work with any
+-- specific student name, use 'Elliot Dickinson' 
+-- for the student's name here.
