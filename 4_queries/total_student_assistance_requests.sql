882dba0e73e4c16a807e6cded1645e6c36b7574f
curriecode <melaniecurrie01@gmail.com>
2019-10-15 16:34:39 -0700

    added: more queries, everything working

--- 4_queries/total_student_assistance_requests.sql
+++ 4_queries/total_student_assistance_requests.sql
@@ -1,18 +1,5 @@
-
-
-
-
-
-
-
-
-
-
--- Get the total number of assistance_requests
--- for a student.
-
--- Select the student's name and the total 
--- assistance requests.
--- Since this query needs to work with any
--- specific student name, use 'Elliot Dickinson' 
--- for the student's name here.
+SELECT count(assistance_requests.*) as total_assistances, students.name
+FROM students
+JOIN assistance_requests ON student_id = students.id
+GROUP BY students.name
+HAVING students.name = 'Elliot Dickinson';
