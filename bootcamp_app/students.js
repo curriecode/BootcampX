30e4b96ec689bec17bcc41974899be35daf2983f
Melanie <melaniecurrie01@gmail.com>
2019-10-17 01:52:22 +0000

    ADDED :  a PostgreSQL database from a JavaScript app using the node-postgres library

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -2,17 +2,21 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'vagrant',
-  password: '123'
+  password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
+
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-LIMIT 5;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
   .then(res => {
-    console.log(res);
-  })
-  .catch(err => console.error('query error', err.stack));
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  }).catch(err => console.error('query error', err.stack));
