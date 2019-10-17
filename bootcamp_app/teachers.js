945956aacb20286df11b7ff73cd6ab35ab73c8fb
Melanie <melaniecurrie01@gmail.com>
2019-10-17 04:54:22 +0000

    UPDATED: to use secure paramatized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+let cohortDate = process.argv[2];
+let limit = process.argv[3] || 5;
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id  = cohorts.id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+LIMIT $2;
+`, [`%${cohortDate}%`, `${limit}`])
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
